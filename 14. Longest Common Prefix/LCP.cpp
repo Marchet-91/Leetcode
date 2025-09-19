a094be1aaf45faf02af072c50414bdb27d543e4f
Marchet-91 <yangrosario2006@gmail.com>
2025-09-19 11:50:42 +0200

    .

---
+++ 14. Longest Common Prefix/LCP.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+    for(int i = 0 ; i < strs[0].size() ; i++){
+        string s = strs[0].substr(0,i+1);
+        for(int j = 1 ; j < strs.size() ; j++){
+            if(strs[j].substr(0,i+1) != s) return s.substr(0,i);
+        }
+    }
+    return strs[0];
+}
+
+int main(){
+    vector<string> strs = {"flower","flow","flight"};
+    
+    cout << longestCommonPrefix(strs);
+
+    return 0;
+}
