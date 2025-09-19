9c99f988dbc8030d25ea64a374ceb793feee443f
Marchet-91 <yangrosario2006@gmail.com>
2025-09-19 16:23:58 +0200

    .

---
+++ 21. Merge Two Sorted Lists/MTSL.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode dummy(0);
+    ListNode* tail = &dummy;
+
+    while (list1 && list2) {
+        if (list1->val <= list2->val) {
+            tail->next = list1;
+            list1 = list1->next;
+        } else {
+            tail->next = list2;
+            list2 = list2->next;
+        }
+        tail = tail->next;
+    }
+
+    // Attacco la lista rimanente (una delle due sarà nullptr)
+    tail->next = (list1 ? list1 : list2);
+
+    return dummy.next;
+}
+
+int main(){
+    
+
+    return 0;
+}
